23e4856cc25c2b0abf29daa37421993e212f93ee
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2022-05-27 19:50:59 +0300

    Add varible to contact-actions for export

--- src/components/redux/contacts-action.js
+++ src/components/redux/contacts-action.js
@@ -1,10 +1,9 @@
 
 import { createAction } from '@reduxjs/toolkit';
 
-const addContact = createAction('contacts/add');
-
-const deleteContact = createAction('contacts/delete');
+ const addContact = createAction('contacts/add');
+ const deleteContact = createAction('contacts/delete');
 const changeFilter = createAction('contacts/changeFilter');
-
-export default { addContact, deleteContact, changeFilter };
+const actions = { addContact, deleteContact, changeFilter };
+export default actions;
 
