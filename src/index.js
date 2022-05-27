e97b91bcc36f427a4fc81352a0c0313bc813b3fe
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2022-05-27 20:46:39 +0300

    Add storage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import store from './components/redux/store';
 import './index.css';
 import App from './App';
@@ -8,9 +9,10 @@ import App from './App';
 ReactDOM.render(
   
   <React.StrictMode>
-     <Provider store={store}>
+    <Provider store={store.store}>
+      <PersistGate loading={null} persistor={store.persistor}>
     <App />
-  
+  </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
